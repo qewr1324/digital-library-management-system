fca336f579dfbc779e79477619f54c58d1c5d857
qewr1324 <naiad.arts.studio@gmail.com>
2026-08-16 21:49:37 +0330

    feat(hibernate-util): update HibernateUtil class
    - Add inPureReturn(), inListReturn() method in class.
    - Add find, findAll static method in class.

--- src/main/java/ir/nas/util/HibernateUtil.java
+++ src/main/java/ir/nas/util/HibernateUtil.java
@@ -1,9 +1,9 @@
 package ir.nas.util;
 
+import java.util.List;
 import java.util.function.Function;
 
 import ir.nas.exception.db.DBConnectionException;
-import ir.nas.model.BaseModel;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
@@ -23,12 +23,22 @@ public final class HibernateUtil
     private HibernateUtil()
     {}
 
-    public static final <T> T persist(final Function<EntityManager, T> function)
+    public synchronized static final <T> T persist(final Function<EntityManager, T> function)
     {
         return new HibernateUtil().inTxReturn(function);
     }
 
-    private final <T> T inTxReturn(final Function<EntityManager, T> function)
+    public synchronized static final <T, ID> T find(final Class<T> clazz, final ID id)
+    {
+        return new HibernateUtil().inPureReturn(clazz, id);
+    }
+
+    public synchronized static final <T> List<T> findAll(final Class<T> clazz, final String query)
+    {
+        return new HibernateUtil().inListReturn(clazz, query);
+    }
+
+    private synchronized final <T> T inTxReturn(final Function<EntityManager, T> function)
     {
         final EntityManager em = emf.createEntityManager();
         final EntityTransaction tx = em.getTransaction();
@@ -60,6 +70,50 @@ public final class HibernateUtil
         } finally {
             if (tx.isActive())
                 tx.rollback();
+        }
+    }
+
+    private synchronized final <T, ID> T inPureReturn(final Class<T> clazz, final ID id)
+    {
+        final EntityManager em = emf.createEntityManager();
+
+        try (em) {
+
+            return em.find(clazz, id);
+
+        } catch (PersistenceException e) {
+
+            e.printStackTrace();
+            throw new DBConnectionException(
+                    ColorCMD.dbError("HibernateUtil Error [isNullReturn()]: ".concat(e.getMessage())));
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            throw new DBConnectionException(
+                    ColorCMD.dbError("HibernateUtil Error [isNullReturn()]: ".concat(e.getMessage())));
+        }
+    }
+
+    private synchronized final <T> List<T> inListReturn(final Class<T> clazz, final String query)
+    {
+        final EntityManager em = emf.createEntityManager();
+
+        try (em) {
+
+            return em.createQuery(query, clazz).getResultList();
+
+        } catch (PersistenceException e) {
+
+            e.printStackTrace();
+            throw new DBConnectionException(
+                    ColorCMD.dbError("HibernateUtil Error [isNullReturn()]: ".concat(e.getMessage())));
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            throw new DBConnectionException(
+                    ColorCMD.dbError("HibernateUtil Error [isNullReturn()]: ".concat(e.getMessage())));
         }
     }
 }
